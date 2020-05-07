db2f761e42714684c042a2cf06e126de1cca9e91
Priyanka Manivannan <47050122+Priyankatech4@users.noreply.github.com>
2020-05-07 13:20:32 +0530

    Add files via upload

---
+++ SpringFrameWork/src/com/xyz/first/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.xyz.first;
+
+public class HelloWorld 
+
+{
+	private String message;
+	private int empid;
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
